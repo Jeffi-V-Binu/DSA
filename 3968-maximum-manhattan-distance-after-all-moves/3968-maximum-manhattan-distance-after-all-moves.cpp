7fd7bf599e4232295150f97953b12cb271c747ff
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-22 22:06:29 +0530

    Time: 4 ms (87.5%), Space: 22.2 MB (87.5%) - LeetHub

---
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDistance(string moves) {
+        int n = moves.size();   
+        int lc = 0;
+        int rc = 0;
+        int uc = 0;
+        int dc = 0;
+        int wc = 0;
+        for(const char& a : moves){
+            if(a == 'U') ++uc;
+            else if(a == 'D') ++dc;
+            else if(a == 'R') ++rc;
+            else if(a == 'L') ++lc;
+            else ++wc;
+        }
+        int maxdis = abs(uc - dc) + abs(lc - rc) + wc;
+        return maxdis;
+    }
+};
