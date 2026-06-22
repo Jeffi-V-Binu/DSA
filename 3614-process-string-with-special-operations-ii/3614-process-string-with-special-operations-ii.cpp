97eb3af371e6618d8f01bab86d8b6cd37c657985
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-22 22:02:39 +0530

    Time: 40 ms (22.6%), Space: 24.2 MB (37.09%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+        int n = s.size();
+        long long m = 0;
+        for(int i = 0 ; i < n ; ++i){
+            if(islower(s[i])){
+                ++m;
+            }
+            else if(s[i] == '*' ){
+                if(m > 0 ) --m;
+            }
+            else if(s[i] == '#'){
+                m*=2;
+            }
+        }
+        if(m <= k) return '.';
+        for(int i = n - 1 ; i >= 0; --i){
+            if(islower(s[i])){
+                --m;
+                if(m == k) return s[i];
+            }
+            else if(s[i] == '*'){
+                ++m;
+            }
+            else if(s[i] == '#'){
+                m = m / 2;
+                k %= m;
+            }
+            else{
+                k = (m - 1) - k;
+            }
+        }
+        return '.';
+    }
+};
