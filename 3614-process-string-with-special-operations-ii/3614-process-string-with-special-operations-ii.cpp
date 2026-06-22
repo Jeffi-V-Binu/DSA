7aedea792b256dfb20cfca5b0b5219a008eb6d76
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-22 22:52:29 +0530

    Time: 23 ms (66.28%), Space: 24.1 MB (60.31%) - LeetHub

--- 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.cpp
@@ -3,7 +3,7 @@ public:
     char processStr(string s, long long k) {
         int n = s.size();
         long long m = 0;
-        for(const char& a : s){
+        for(const char a : s){
             if(a == '%'){
                 continue;
             }
@@ -19,13 +19,14 @@ public:
         }
         if(k + 1 > m) return '.';
         for(int i = n - 1 ; i >= 0; --i){
-            if(s[i] == '%'){
+            char a = s[i];
+            if(a == '%'){
                 k = (m - 1) - k;
             }
-            else if(s[i] == '*'){
+            else if(a == '*'){
                 ++m;
             }
-            else if(s[i] == '#'){
+            else if(a == '#'){
                 if(k + 1 > (m + 1) / 2){
                     k -= (m / 2);
                 }
