5529c3d65c1830547cab66ffa9b226cd2b9f84a4
Jeffi V Binu <jeffibinu@gmail.com>
2026-06-27 23:32:54 +0530

    Time: 54 ms (86.86%), Space: 103.6 MB (90.29%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
     int maximumLength(vector<int>& nums) {
-        map<int , int> frequency ;
         long long track;
+        const int maxval = 31623;
         int length = 0;
         int maxlen = 1;
+        int mi = maxval;
+        int ma = 0;
         int n = nums.size();
+        int len1 = 0;
+        bitset<maxval> frequency , freq2 , largsqs;
         for(int i = 0 ; i < n ; ++i){
-            frequency[nums[i]] ++;
-        }
-        if(frequency.find(1) != frequency.end()){
-            length = frequency[1];
-            if(length % 2 == 0){
-                --length;
-            }
-            maxlen = max(maxlen , length);
-            length = 0;
-        }
-        for(const auto& [base , count] : frequency){
-            track = base;
-            if(track == 1) continue;
-            while(frequency.find(track) != frequency.end() && frequency[track] >= 2 && track < 1000000000){
-                    track *= track;
-                    length += 2;
+            if(nums[i] == 1) ++len1;
+            if(nums[i] < maxval){
+                if(frequency[nums[i]] == 0){
+                    frequency[nums[i]] = 1;
                 }
-            if(frequency.find(track) != frequency.end() && frequency[track] == 1) ++length;
+                else{
+                    freq2[nums[i]] = 1;
+                    ma = max(ma , nums[i]);
+                    mi = min(mi , nums[i]);
+                }
+            }
+            else{
+                int x = sqrt(nums[i]);
+                if(x * x == nums[i]){
+                    largsqs[x] = 1;
+                }
+            }
+        }
+        for(int i = mi ; i <= ma ; ++i){
+            track = i;
+            if(track == 1) continue;
+            while(track < maxval && freq2[track]){
+                length += 2;
+                track *= track;
+            }
+            if(track < maxval && frequency[track]) ++length;
+            else if(track >= maxval){
+                int d = sqrt(track);
+                if(largsqs[d]) ++length;
+                else --length;
+            }
             else --length;
             maxlen = max(maxlen , length);
             length = 0;
         }
+        if(len1 % 2 == 0) --len1;
+        maxlen = max(maxlen , len1);
         return maxlen;
     }
 };
