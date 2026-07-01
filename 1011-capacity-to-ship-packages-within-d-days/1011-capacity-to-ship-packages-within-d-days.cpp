bb5bb99365de022820084c545713ab43ad10f488
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-01 21:57:37 +0530

    Time: 12 ms (51.35%), Space: 35.1 MB (19.44%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int minw = -10000;
+        int maxw = 0;
+        for(const int& i : weights){
+            minw = max(minw , i);
+            maxw += i;
+        }
+        int mid;
+        while(minw < maxw){
+            mid = minw + (maxw - minw) / 2;
+            int mind = 1;
+            int total = 0;
+            for(const int& i : weights){
+                if(total + i > mid){
+                    total = i;
+                    ++mind;
+                }
+                else{
+                    total += i;
+                }
+            }
+            if(mind > days){
+                minw = mid + 1;
+            }
+            else{
+                maxw = mid;
+            }
+        }
+        return maxw;
+    }
+};
