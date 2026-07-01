b5676b8ef513b2e18367424eea4b2a2e4c5e37ed
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-01 20:58:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (81.7%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int>pathsdp(n , 1);
+        if(m < 2 || n < 2) return 1;
+        vector<int>pathsdp(n , 1);        
         for(int i = 1 ; i < m ; ++i){
             for(int j = 1 ; j < n ; ++j){
                 pathsdp[j] = pathsdp[j] + pathsdp[j - 1];
