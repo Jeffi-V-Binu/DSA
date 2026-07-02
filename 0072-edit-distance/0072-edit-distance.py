9a3f04ad42d4a4e44080bda653d6a9b238958359
Jeffi V Binu <jeffibinu@gmail.com>
2026-07-02 12:24:02 +0530

    Time: 55 ms (29.76%), Space: 22.5 MB (83.5%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        table = [[0] * (len(word2)+1 )for i in range(len(word1)+1)]
+        for i in range(len(word2)):
+            table[0][i + 1] = i + 1
+        for i in range(len(word1)):
+            table[i + 1][0] = i + 1
+        for i in range(1 , len(word1)+1):
+            for j in range(1 , len(word2) + 1):
+                if(word1[i - 1] == word2[j - 1]):
+                    table[i][j] = table[i-1][j-1]
+                else:
+                    table[i][j] = min(table[i - 1][j] , table[i - 1][ j - 1], table[i][j - 1]) + 1
+        return table[len(word1)][len(word2)]
